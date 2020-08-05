7c5c2410d78dcd9a78902e75ece3b3ff8bf79b6e
Li <wli@wlilt2-osx.local>
2020-08-04 22:54:09 -0700

    add Docker option

---
+++ Docker/server-config-docker.pl
@@ -0,0 +1,17 @@
+#### EC2 setting ###################################################################### 
+$NGS_cdhit_dir = "/opt/cd-hit";
+$NGS_blast_dir = "/opt/ncbi-blast-2.8.1+/bin";
+$SL_session_dir     = "/opt/data";
+$SL_rammcap_dir     = "";
+$SL_bin_dir         = "";
+
+$qsub_exe = "/bin/bash";
+$admin_email = "liwz\@sdsc.edu";
+$sw_db = "";
+$pdb_db = "";
+
+$qsub_local = <<EOD;
+
+EOD
+
+
